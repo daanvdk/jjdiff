456b7dd7dd4909c5a7e49719f9ae89a49f976306
Daan van der Kallen <mail@daanvdk.com>
2025-08-25 12:34:09 +0300

    Add some more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 from collections.abc import Iterator
 from contextlib import ExitStack
 from pathlib import Path
-import stat
 from unittest.mock import patch
 import tempfile
 
@@ -9,7 +8,7 @@ import pytest
 
 from jjdiff.config import Config
 
-from .types import ExecFile, TempDirSpec, TempDirFactory
+from .utils import DirSpec, DirFactory, write_spec
 
 
 # Always use default config in tests
@@ -23,34 +22,13 @@ def config() -> Iterator[Config]:
 
 
 @pytest.fixture
-def temp_dir_factory() -> Iterator[TempDirFactory]:
+def temp_dir_factory() -> Iterator[DirFactory]:
     stack = ExitStack()
 
-    def temp_dir_factory(dir_spec: TempDirSpec) -> Path:
-        dir_path = Path(stack.enter_context(tempfile.TemporaryDirectory()))
-
-        for path, file_spec in dir_spec.items():
-            file_path = dir_path / path
-            file_path.parent.mkdir(exist_ok=True, parents=True)
-
-            if isinstance(file_spec, ExecFile):
-                file_spec = file_spec.file
-                is_exec = True
-            else:
-                is_exec = False
-
-            match file_spec:
-                case str():
-                    file_path.write_text(file_spec)
-                case bytes():
-                    file_path.write_bytes(file_spec)
-                case Path():
-                    file_path.symlink_to(file_spec)
-
-            if is_exec:
-                file_path.chmod(file_path.stat().st_mode | stat.S_IXUSR)
-
-        return dir_path
+    def temp_dir_factory(dir_spec: DirSpec) -> Path:
+        root = Path(stack.enter_context(tempfile.TemporaryDirectory()))
+        write_spec(root, dir_spec)
+        return root
 
     with stack:
         yield temp_dir_factory
---
+++ tests/test_change.py
@@ -0,0 +1,78 @@
+from pathlib import Path
+from jjdiff.diff import diff
+from jjdiff.change import (
+    AddFile,
+    ChangeRef,
+    DeleteFile,
+    Line,
+    LineRef,
+    ModifyFile,
+    apply_changes,
+    reverse_changes,
+    split_changes,
+)
+
+from .utils import DirFactory, read_spec
+
+
+def test_apply_nothing(temp_dir_factory: DirFactory) -> None:
+    root = temp_dir_factory({})
+    apply_changes(root, [])
+    assert read_spec(root) == {}
+
+
+def test_add_file(temp_dir_factory: DirFactory) -> None:
+    root = temp_dir_factory({})
+    changes = [
+        AddFile(Path("foo.txt"), [Line(None, "foo")], False),
+    ]
+    apply_changes(root, changes)
+    assert read_spec(root) == {"foo.txt": "foo"}
+
+
+def test_delete_file(temp_dir_factory: DirFactory) -> None:
+    root = temp_dir_factory({"foo.txt": "foo"})
+    changes = [
+        DeleteFile(Path("foo.txt"), [Line("foo", None)], False),
+    ]
+    apply_changes(root, changes)
+    assert read_spec(root) == {}
+
+
+def test_modify_file(temp_dir_factory: DirFactory) -> None:
+    root = temp_dir_factory({"foo.txt": "foo"})
+    changes = [
+        ModifyFile(Path("foo.txt"), [Line("foo", "bar")]),
+    ]
+    apply_changes(root, changes)
+    assert read_spec(root) == {"foo.txt": "bar"}
+
+
+def test_usecase(temp_dir_factory: DirFactory) -> None:
+    old = temp_dir_factory({"foo.txt": "foo\nbar"})
+    new = temp_dir_factory({"foo.txt": "fooo\nbaz", "bar.txt": "barrr"})
+
+    old_to_new = diff(old, new)
+    assert old_to_new == [
+        AddFile(Path("bar.txt"), [Line(None, "barrr")], False),
+        ModifyFile(Path("foo.txt"), [Line("foo", "fooo"), Line("bar", "baz")]),
+    ]
+
+    selection = {ChangeRef(1), LineRef(1, 1)}
+    old_to_sel, sel_to_new = split_changes(old_to_new, selection)
+    assert old_to_sel == [
+        ModifyFile(Path("foo.txt"), [Line("foo", "foo"), Line("bar", "baz")]),
+    ]
+    assert sel_to_new == [
+        AddFile(Path("bar.txt"), [Line(None, "barrr")], False),
+        ModifyFile(Path("foo.txt"), [Line("foo", "fooo"), Line("baz", "baz")]),
+    ]
+
+    new_to_sel = list(reverse_changes(sel_to_new))
+    assert new_to_sel == [
+        DeleteFile(Path("bar.txt"), [Line("barrr", None)], False),
+        ModifyFile(Path("foo.txt"), [Line("fooo", "foo"), Line("baz", "baz")]),
+    ]
+
+    apply_changes(new, new_to_sel)
+    assert read_spec(new) == {"foo.txt": "foo\nbaz"}
